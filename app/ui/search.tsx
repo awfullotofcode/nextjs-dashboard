b05080f1883fb409ee7c6d506d814a42d5cb031f
awfullotofcode <gomezab04@gmail.com>
2024-06-06 15:14:47 -0600

    added searchParams prop to page in order to retrieve and use URL params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,11 +1,32 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
+    // create new URLSearchParams instance
+    const params = new URLSearchParams(searchParams);
+    // set params based on user input, if empty delete
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    /* update URL with user's search data
+       ${pathname} = current path (e.g. 'dashboard/invoices' 
+       ${params.toString()} = translates user input into URL friendly
+       format as user typesinto search bar 
+    */
+    replace(`${pathname}?${params.toString()}`);
     console.log(term);
   }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -17,6 +38,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
