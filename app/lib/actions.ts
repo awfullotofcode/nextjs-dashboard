9cc4322621784b2d088a8a73388f830fc140a881
awfullotofcode <gomezab04@gmail.com>
2024-06-06 23:59:53 -0600

    passed id to server function in order to update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true });
+const UpdateInvoice = FormSchema.omit({id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
 
@@ -38,5 +39,23 @@ export async function createInvoice(formData: FormData) {
     // clear cache and trigger new request to server
     revalidatePath('/dashboard/invoices');
     // redirect user
+    redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerid'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}`
+
+    revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
