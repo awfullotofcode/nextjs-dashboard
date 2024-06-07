e93ca8317a77541ecb45c4b5a5f4af6ce82df441
awfullotofcode <gomezab04@gmail.com>
2024-06-06 21:53:02 -0600

    use .get() to extract values from formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,12 @@
 'use server';
 
-export async function createInvoice(formData: FormData){}
+export async function createInvoice(formData: FormData){
+
+    //use .get(name) to extract values from formData
+    const rawFormData = {
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    console.log(rawFormData);
+}
